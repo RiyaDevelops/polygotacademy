d14eb0a15600bc886c857e06e1359f60574cc82a
Riya <riyadevelops01@gmail.com>
2024-05-07 16:40:25 -0400

    courses section

--- js/main.js
+++ js/main.js
@@ -32,3 +32,47 @@
 
 
 
+
+// Back to Top Button 
+function scrollToBottom() {
+    window.scrollTo({  //scrollTo method used in js 
+        bottom: 0,
+        behavior: 'smooth'
+    });
+}
+
+var prevScrollpos = window.pageYOffset;
+
+window.addEventListener('scroll', function() {
+    var currentScrollPos = window.pageYOffset;
+    var goBottomButton = document.querySelector('.go-to-bottom');
+
+    if (prevScrollpos > currentScrollPos) {
+        goBottomButton.style.display = "inline-flex";
+    } else {
+        goBottomButton.style.display = "none";
+    }
+    prevScrollpos = currentScrollPos;
+});
+
+
+// Back to Top Button 
+function scrollToTop() {
+    window.scrollTo({  //scrollTo method used in js 
+        top: 0,
+        behavior: 'smooth'
+    });
+}
+
+window.addEventListener('scroll', myHeight)
+ function myHeight(){
+    var goTopButton= document.querySelector('.back-to-top'); //selecting the class
+    var windowHeight = window.innerHeight; //window.innerHeight represents the height of the viewport (the visible area of the browser window).
+    var documentHeight = document.documentElement.scrollHeight; // Total height of the document
+
+    if(window.scrollY +  windowHeight >= documentHeight){ //When the sum is greater than or equal to the document height, it means the user has scrolled to the bottom of the page.
+        goTopButton.style.display = "inline-flex"
+    } else{
+        goTopButton.style.display = "none"
+    }
+ }
