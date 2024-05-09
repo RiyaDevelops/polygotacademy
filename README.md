0dba6735f2ab8f41d778f8229ab3c965f254d2a8
Riya <riyadevelops01@gmail.com>
2024-05-08 20:39:52 -0400

    design stage

--- README.md
+++ README.md
@@ -11,3 +11,17 @@
 
 # accessibility  
 # https://a11y-guidelines.orange.com/en/articles/accessible-hiding/
+
+# Illustrations 
+# https://undraw.co/illustrations
+
+
+
+# Fixes to be done 
+# fix hero section bottom area 
+# reduce line and bring names close to tetsimonaila paragrah 
+# fix color scheme
+# fix padding of buttons 
+# get responsive images 
+# fix padding and margin 
+# Photoshop logo image on photopea
--- js/main.js
+++ js/main.js
@@ -1,34 +1,4 @@
-// hamburger variable, hamburger variable selects css element with class name menu
-// JavaScript code creates an event listener for the click event on the hamburger menu icon and toggles the "navbar-open" class on the mobile menu
-// document.addEventListener('DOMContentLoaded', function () {
-//     const hamburger = document.querySelector(".mobile-navbar-menu");
-//     const mobileMenu = document.querySelector(".mobile");
-//     const hamburgerIcon = document.querySelector(".mobile-navbar-menu i");
-//     const navLinks = document.querySelectorAll(".nav__link");
-
-//     // Toggle mobile menu and hamburger icon
-//     hamburger.addEventListener('click', () => {
-//         mobileMenu.classList.toggle("navbar-open");
-
-//         // Toggle the hamburger icon between bars and x
-//         if (mobileMenu.classList.contains("navbar-open")) {
-//             hamburgerIcon.classList.remove("fa-bars");
-//             hamburgerIcon.classList.add("fa-times");
-//         } else {
-//             hamburgerIcon.classList.remove("fa-times");
-//             hamburgerIcon.classList.add("fa-bars");
-//         }
-//     });
-
-//     // Close mobile menu when a navigation link is clicked
-//     navLinks.forEach(link => {
-//         link.addEventListener("click", () => {
-//             mobileMenu.classList.remove("navbar-open");
-//             hamburgerIcon.classList.remove("fa-times");
-//             hamburgerIcon.classList.add("fa-bars");
-//         });
-//     });
-// });
+// hamburger variable menu
 
 
 
