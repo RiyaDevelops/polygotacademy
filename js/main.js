ac15810b4a0e258ddd0733ce6171c3cd5658175f
Riya <riyadevelops01@gmail.com>
2024-05-15 16:55:33 -0400

    fixed button responsiveness

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+
+
+// console.log("JavaScript file loaded successfully!");
+
 // Hamburger Responsive Navbar
 
 // JavaScript event listener
@@ -41,23 +45,23 @@
 //     });
 
 
-// console.log("JavaScript file loaded successfully!");
 
+// FREQUENTLY ASKED QUESTIONS - ACCORDIAN 
 
-var acc = document.getElementsByClassName("accordion");
-var i;
+// var acc = document.getElementsByClassName("accordion");
+// var i;
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
-  });
-}
+// for (i = 0; i < acc.length; i++) {
+//   acc[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     var panel = this.nextElementSibling;
+//     if (panel.style.maxHeight) {
+//       panel.style.maxHeight = null;
+//     } else {
+//       panel.style.maxHeight = panel.scrollHeight + "px";
+//     } 
+//   });
+// }
 
 
 
